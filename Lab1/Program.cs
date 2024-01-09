0f89a01a53e06e935a023b44dde67443da51d910
Teinne <nq2019thaigiabao230504@gmail.com>
2024-01-09 17:35:40 +0700

    update

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,21 +10,23 @@ namespace Lab1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //string filePath = @"D:\22DH110310__ThaiGiaBao\txt\matran.txt";
-            //int n;
-            //int[,] adjMatrix;
-            //ReadAdjMatrix(filePath,out n,out adjMatrix);
-            //OutPut(adjMatrix);
-            //BacCuaDinh(adjMatrix);
-            //TestVaoRa();
-            //TestDanhSachKe();
-            //TestVaoRa();
-            //TestDanhSachKe();
-            TestDSCanh();
-            Console.ReadKey();
-        }
+            static void Main(string[] args)
+            {
+                //string filePath = @"D:\22DH110310__ThaiGiaBao\txt\matran.txt";
+                //int n;
+                //int[,] adjMatrix;
+                //ReadAdjMatrix(filePath,out n,out adjMatrix);
+                //OutPut(adjMatrix);
+                //BacCuaDinh(adjMatrix);
+                //TestVaoRa();
+                //TestDanhSachKe();
+                //TestVaoRa();
+                //TestDanhSachKe();
+                //TestDSCanh();
+                //TestCanh2Ke();
+                TestDanhSachKe2Canh();
+                Console.ReadKey();
+            }
 
 
         static void TestVaoRa()
@@ -48,11 +50,42 @@ namespace Lab1
 
         static void TestDSCanh()
         {
-            string filePath = @"D:\22DH110310__ThaiGiaBao\txt\DSke.txt";
+            string filePath = @"D:\22DH110310__ThaiGiaBao\txt\DSCanh.txt";
             int n;
             List<Tuple<int, int>> adjMatrix;
             ReadAdjMatrixTuple(filePath, out n, out adjMatrix);
             DanhSachCanh(adjMatrix);
+        }
+
+        static void TestCanh2Ke()
+        {
+            string filePath = @"D:\22DH110310__ThaiGiaBao\txt\Canh2Ke.txt";
+            int n;
+            int m;
+            List<Tuple<int, int>> edgeList;
+            ReadEdgeList(filePath, out n, out m, out edgeList);
+            List<int>[] adjList = new List<int>[m];
+            adjList = ConvertEdgeList2AdjList(n,m,edgeList,adjList);
+            for (int i = 0; i < adjList.Length; i++)
+            {
+                var result = string.Join(" ", adjList[i]);
+                Console.WriteLine(result);
+            }
+
+        }
+
+        static void TestDanhSachKe2Canh()
+        {
+            string filePath = @"D:\22DH110310__ThaiGiaBao\txt\DSKe2Canh.txt";
+            int n;
+            int m;
+            List<int>[] adjList;
+            ReadAdjMatrixMultiple(filePath, out n, out adjList);
+            List<Tuple<int, int>> edgeList;
+            ConvertAdjList2EdgeList(n, out m, out edgeList, adjList);
+           
+
+
         }
 
         static void ReadAdjMatrix(string filePath, out int n, out int[,] adjMatrix)
@@ -75,16 +108,12 @@ namespace Lab1
         {
             StreamReader sr = new StreamReader(filePath);
             n = Convert.ToInt32(sr.ReadLine());
-            adjList = new List<int>[n];
-            for (int u = 0; u < n; u++)
+            adjList = new List<int>[n+1];
+            for (int u = 1; u <= n; u++)
             {
                 string line = sr.ReadLine();
                 string[] nums = line.Split();
-                adjList[u] = new List<int>();
-                for (int v = 0; v < nums.Length; v++)
-                {
-                    adjList[u].Add(int.Parse(nums[v]));
-                }
+                adjList[u] = Array.ConvertAll(nums, s => int.Parse(s)).ToList();
             }
         }
 
@@ -100,6 +129,28 @@ namespace Lab1
                 adjTuple.Add(new Tuple<int, int>(int.Parse(nums[0]), int.Parse(nums[1])));
             }
         }
+
+        static void ReadEdgeList(string path, out int n, out int m, out List<Tuple<int, int>> edgeList)
+        {
+            StreamReader sr = new StreamReader(path);
+            string line = sr.ReadLine();
+            string[] values = line.Split();
+            n = Convert.ToInt32(values[0]);
+            m = Convert.ToInt32(values[1]);
+            edgeList = new List<Tuple<int, int>>();
+
+            for (int i = 0; i < m; i++)
+            {
+                line = sr.ReadLine();
+                values = line.Split();
+                int u = Convert.ToInt32(values[0]);
+                int v = Convert.ToInt32(values[1]);
+                Tuple<int, int> e = new Tuple<int, int>(u, v);
+                edgeList.Add(e);
+            }
+            sr.Close();
+        }
+
         static void BacCuaDinh(int[,] adjMatrix)
         {
             for (int i = 0; i < adjMatrix.GetLength(0); i++)
@@ -159,7 +210,40 @@ namespace Lab1
             }
             foreach (int count in result)
             {
-                Console.WriteLine($"{count}");
+                Console.Write(count + " ");
+            }
+        }
+
+        static List<int>[] ConvertEdgeList2AdjList(int n, int m, List<Tuple<int, int>> edgeList, List<int>[] adjList)
+        {
+            adjList = new List<int>[m];
+            for (int i = 0; i < adjList.Length; i++)
+            {
+                adjList[i] = new List<int>();
+            }
+            foreach(var e in edgeList)
+            {
+                adjList[e.Item1].Add(e.Item2);
+                adjList[e.Item2].Add(e.Item1);
+            }
+            return adjList;
+        }
+
+        static void ConvertAdjList2EdgeList(int n, out int m, out List<Tuple<int, int>> edgeList, List<int>[] adjList)
+        {
+            m = n;
+            edgeList = new List<Tuple<int, int>>();
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 0; j < adjList[i].Count; j++)
+                {
+                    Tuple<int,int> e = new Tuple<int, int>(i, adjList[i][j]);
+                    if(i < adjList[i][j])
+                    {
+                        edgeList.Add(e);
+                        Console.WriteLine(e);
+                    }
+                }
             }
         }
 
